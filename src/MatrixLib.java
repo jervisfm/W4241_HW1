5f6094d9b102217f241d55a92e683ff6d433d3ab
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-01-29 00:34:02 -0500

    start work on implementing the matrix algs

--- src/MatrixLib.java
+++ src/MatrixLib.java
@@ -1,7 +1,34 @@
 
 public class MatrixLib {
 
-	public static strasMult(Matrix a, Matrix b) {
+	/**
+	 * Does Matrix Multiplication using Strassen Algorithm 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static Matrix strasMult(Matrix a, Matrix b) {
+		
+	}
+	
+	/**
+	 * Performs Matrix Multiplication using the traditional
+	 * (naive) algorithm. 
+	 * 
+	 * @param a - matrix A
+	 * @param b - Matrix B
+	 * @return Matrix A multiplied by Matrix b, or 
+	 * NULL if an error occurs. 
+	 */
+	public static Matrix mult(Matrix a, Matrix b) {
+		
+		// Check inputs
+		if (!MatrixUtil.canMultiply(a, b) ||
+			a == null || b == null)
+			return null;
+		
+		
+		
 		
 	}
 }
