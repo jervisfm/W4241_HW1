da1f9db27483ab7275d6718e58ce5ae05f58f863
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-01-29 00:07:04 -0500

    work on matrix class and add a few starter tests

--- src/Matrix.java
+++ src/Matrix.java
@@ -6,11 +6,24 @@ public class Matrix {
 	public Matrix(int rows, int cols) {
 		this.data = new double[rows][cols];
 	}
-	
-	public double get(int row, int col){
+			
+	public double get(int row, int col) throws Exception{		
+		
+		checkIndexRange(row, col);
+			
 		return data[row][col];
 	}
-		
+
+	
+	public void set(double[][] data) {
+		this.data = data; 
+	}
+	
+	public double set(int row, int col, double value) throws Exception {
+		checkIndexRange(row, col);
+		return data[row][col] = value;
+	}
+	
 	public int rowSize() {
 		return data.length;
 	}
@@ -24,8 +37,25 @@ public class Matrix {
 	}
 	
 	public String toString() {
-		
 		return super.toString();
 	}
+	/**
+	 * Checks if the given indice values are valid
+	 * @param row - row
+	 * @param col - column
+	 * @return - true on success 
+	 * @throws Exception an exception if range is invalid
+	 */
+	private boolean checkIndexRange(int row, int col) throws Exception {
+		int rowSize = rowSize(); 
+		int colSize = colSize(); 
 		
+		if (row >= rowSize || col >= colSize ||
+			row  < 0       || col < 0)  {
+			throw new Exception(String.format("Index Out of Range (%d,%d)",
+								row, col));
+		} else {
+			return true;
+		}
+	}
 }
--- src/MatrixTest.java
+++ src/MatrixTest.java
@@ -6,8 +6,34 @@ import org.junit.Test;
 public class MatrixTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void CreateMatrix() {
+		double test[][] = { {1,2}, {3,4} };
+		Matrix m = new Matrix(2,2);
+		m.set(test); 
+		double[][] actual = m.data(); 
+		assertArrayEquals(test, actual);
+	}
+	
+	
+	@Test
+	public void SetMatrix() {
+		double test[][] = { {1,2}, {3,4} };
+		Matrix m = new Matrix(2,2); 
+		m.set(test);
+		
+		// set a value and check it.
+		double expected = 767;
+		double actual;
+		try {
+			m.set(1, 1, expected);
+			actual = m.get(1, 1);
+			assertEquals(expected, actual, Math.pow(10, -6));
+		} catch (Exception e) {
+			fail("Incorrect Out of Index error:" + e);
+		}
+
+		
+		
 	}
 
 }
